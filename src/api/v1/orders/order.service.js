67080dc97e28e7b184d1eca7a85ab3c4911cb247
ilohec <chukwuneme@gmail.com>
2025-08-21 12:36:58 +0100

    webhook error and ratelimit fixx3.1_unsure

--- src/api/v1/orders/order.service.js
+++ src/api/v1/orders/order.service.js
@@ -11,7 +11,6 @@ const HttpError = require('../../../utils/HttpError');
 const { firestore, admin, isFirebaseInitialized } = require('../../../config/firebase.config.js');
 const { logger } = require('../../../config/logger.config.js');
 const referralService = require('../referrals/referral.service');
-const { sendOrderStatusUpdate } = require('../../../services/fcm.service');
 
 // NEW: Added Dependencies from O2
 const paymentService = require('../payments/payment.service'); 
